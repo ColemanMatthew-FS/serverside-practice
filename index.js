db302e559fe204e9a35173a3a9e1794a9353f5b3
Matthew Coleman <matthewcoleman0066@gmail.com>
2023-09-04 09:08:39 -0400

    Installed a templating engine

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express()
 //imports the quizzes controller
 const quizzes = require('./controllers/quizzes')
 
-app.get('/', (request, response) => {
-    response.send('Home Page! GET...')
-})
+//all our view templates are in the views folder
+app.set('views', __dirname + '/views')
+app.set('view engine', 'twig')
 
-app.post('/', (request, response) => {
-    response.send('Home Page! POST...')
+app.get('/', (request, response) => {
+    response.render('home')
 })
 
 //everything after the : is a parameter called productName
@@ -18,6 +18,7 @@ app.get('/products/:productName', (request, response) => {
 })
 
 //allows users to request data from localhost:3000/quizzes
+//"'/quizzes" allows us to use urls ending in "/quizzes", the "quizzes" variable we set earlier tells the app to use the controllers/quizzes router
 app.use('/quizzes', quizzes)
 
 app.listen(3000)
