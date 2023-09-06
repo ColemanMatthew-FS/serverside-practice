75278612942f2005af3a5b345b83974324bd6b97
Matthew Coleman <matthewcoleman0066@gmail.com>
2023-09-06 12:30:52 -0400

    Added a form to edit/update quizzes, added pages to view quizzes, updated routes to work with twig templates

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 const express = require('express')
 const app = express()
 //imports the quizzes controller
-const quizzes = require('./src/controllers/quizzes')
-const { Quiz } = require('./src/models')
+const quizzesCtrl = require('./src/controllers/quizzes')
 
 //all our view templates are in the views folder
 app.set('views', __dirname + '/src/views')
 app.set('view engine', 'twig')
 
 app.get('/', async (request, response) => {
-    const quiz = await Quiz.findByPk(1)
-    response.render('home/home', {
-        quiz
-    })
-})
-
-//everything after the : is a parameter called productName
-//going to localhost:3000/products/foo returns "Product page! Product name: foo"
-app.get('/products/:productName', (request, response) => {
-    response.send('Product page! Product name: '+ request.params.productName)
+    response.render('home/home')
 })
 
 //allows users to request data from localhost:3000/quizzes
 //"'/quizzes" allows us to use urls ending in "/quizzes", the "quizzes" variable we set earlier tells the app to use the controllers/quizzes router
-app.use('/quizzes', quizzes)
+app.use('/quizzes', quizzesCtrl)
 
 app.listen(3000)
